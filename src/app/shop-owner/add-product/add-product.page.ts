254612c60aff30f33a86e5be1be79ff89cd4b6bf
Samarpreet Singh <bhamrasamar02@gmail.com>
2025-11-05 14:41:27 +0530

    Build add-product form page with product details inputs and toggle for stock status

--- src/app/shop-owner/add-product/add-product.page.ts
+++ src/app/shop-owner/add-product/add-product.page.ts
@@ -1,20 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonicModule } from '@ionic/angular';
 
 @Component({
   selector: 'app-add-product',
   templateUrl: './add-product.page.html',
   styleUrls: ['./add-product.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonicModule, CommonModule, FormsModule]
 })
 export class AddProductPage implements OnInit {
+  public newProduct = {
+    name: '',
+    price: null,
+    description: '',
+    inStock: true
+  };
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  public saveProduct(): void {
+    console.log('Saving product:', this.newProduct);
   }
 
 }
