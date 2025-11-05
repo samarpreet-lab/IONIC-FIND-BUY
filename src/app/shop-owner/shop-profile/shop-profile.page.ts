f6b997fc8438d12c8f957dd126ac5b3ef53632a6
Samarpreet Singh <bhamrasamar02@gmail.com>
2025-11-05 14:31:38 +0530

    Build shop owner shop profile page with editable form for shop details

--- src/app/shop-owner/shop-profile/shop-profile.page.ts
+++ src/app/shop-owner/shop-profile/shop-profile.page.ts
@@ -1,20 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonicModule } from '@ionic/angular';
 
 @Component({
   selector: 'app-shop-profile',
   templateUrl: './shop-profile.page.html',
   styleUrls: ['./shop-profile.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonicModule, CommonModule, FormsModule]
 })
 export class ShopProfilePage implements OnInit {
+  public shopProfile = {
+    name: 'My Local Shop',
+    address: '123 Main St, City',
+    phone: '9876543210',
+    hours: '9:00 AM - 6:00 PM',
+    description: 'We sell the best local products!'
+  };
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  public saveChanges(): void {
+    console.log(this.shopProfile);
   }
 
 }
